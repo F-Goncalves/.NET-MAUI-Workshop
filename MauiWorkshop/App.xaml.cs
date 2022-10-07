ea5ef8ab8dafa338b9ed53a0183bc1fb5f4691c7
Flavio Goncalves <f.goncalves@cayas.de>
2022-10-07 14:04:35 +0200

    Initial create from template

---
+++ MauiWorkshop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiWorkshop;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiWorkshop/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiWorkshop;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
