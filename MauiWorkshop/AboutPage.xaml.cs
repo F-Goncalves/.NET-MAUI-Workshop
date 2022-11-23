7d6a9c1e1c9027632b2ae88a0d8391ba447a8346
Flavio Goncalves <flavio.goncalves@outlook.de>
2022-11-23 16:45:20 +0100

    Switch from Shell to classic Navigation with FlyoutPage

---
+++ MauiWorkshop/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiWorkshop;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiWorkshop/RootFlyoutPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiWorkshop;
+
+public partial class RootFlyoutPage
+{
+	public RootFlyoutPage()
+	{
+		InitializeComponent();
+	}
+
+    void Characters_Clicked(System.Object sender, System.EventArgs e)
+    {
+        this.Detail = new NavigationPage(new MainPage());
+    }
+
+    void About_Clicked(System.Object sender, System.EventArgs e)
+    {
+        this.Detail = new NavigationPage(new MainPage());
+    }
+}
